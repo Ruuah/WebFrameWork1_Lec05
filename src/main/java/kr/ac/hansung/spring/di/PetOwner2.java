d19fd773292d22ebc5a81b225af7b04c85bc5662
Haruu <Haruu@Haruu-PC>
2017-10-12 11:03:51 +0900

    Dependency Injection

---
+++ src/main/java/kr/ac/hansung/spring/di/PetOwner2.java
@@ -0,0 +1,21 @@
+/* Example Using annotation */
+package kr.ac.hansung.spring.di;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class PetOwner2 {
+
+	@Autowired
+	@Qualifier(value="qf_dog")
+	/* Wiring by Type, Type에 맞는 bean이 자동으로 주입된다
+	 * 주입되는 값이 여러 개면 null이 들어간다
+	 * qualifer로 어떤 값을 넣을지 명시해준다
+	 */
+	private AnimalType animal;
+
+	public void play() {
+		animal.sound();
+	}
+	
+}
